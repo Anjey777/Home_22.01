84334cb266e3ecb64056b6185e64477aeee6ebf8
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2023-01-26 20:52:12 +0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,8 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+number = 5
+function triangleNumber(number){
+    return 0.5 * number * (number + 1)
+}
+print (number)
